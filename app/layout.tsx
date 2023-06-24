8221c0ca7d01f13b66a785318d398273d2c53047
许佳豪 <10351874+xujiahao29@user.noreply.gitee.com>
2023-06-24 19:34:30 +0800

    modify title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
 
 export const metadata = {
-  title: "ChatGPT国内版免费使用",
+  title: "FreeGPT4",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
@@ -17,7 +17,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT国内版免费使用",
+    title: "FreeGPT4",
     statusBarStyle: "default",
   },
 };

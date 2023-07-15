e9203f2bc5bbcb0e914eacf5f85bad32bb683ed0
duandingxin <121155395+duandingxin@users.noreply.github.com>
2023-07-16 00:05:38 +0800

    Update config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -71,23 +71,23 @@ export const ALL_MODELS = [
   },
   {
     name: "gpt-3.5-turbo",
-    available: true,
+    available: ENABLE_GPT4,
   },
   {
     name: "gpt-3.5-turbo-0301",
-    available: true,
+    available: ENABLE_GPT4,
   },
   {
     name: "gpt-3.5-turbo-0613",
-    available: true,
+    available: ENABLE_GPT4,
   },
   {
     name: "gpt-3.5-turbo-16k",
-    available: true,
+    available: ENABLE_GPT4,
   },
   {
     name: "gpt-3.5-turbo-16k-0613",
-    available: true,
+    available: ENABLE_GPT4,
   },
   {
     name: "qwen-v1", // 通义千问

ab44ee9e0a2422bca3ca9ce21514035336c27351
cyZ52 <1349706931@qq.com>
2023-11-08 22:14:16 +0800

    11-8

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { Analytics } from "@vercel/analytics/react";
 
 import { Home } from "./components/home";
-import styles from "./styles/globals.scss";
 import { getServerSideConfig } from "./config/server";
 
 const serverConfig = getServerSideConfig();
